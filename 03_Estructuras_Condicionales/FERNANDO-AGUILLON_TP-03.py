b84e542cd88f6b07caad1e5510993151695ce002
Fernando <fernando.j.aguillon.b@gmail.com>
2025-04-20 12:12:35 -0300

    Ejercicios resueltos

---
+++ 03_Estructuras_Condicionales/FERNANDO-AGUILLON_TP-03.py
@@ -0,0 +1,37 @@
+# Ejercicio 1
+#edad = int(input("Ingrese su edad: "))
+#if edad > 18 :
+#    print("Es mayor de edad")
+
+# Ejercicio 2
+# nota = int(input("Ingrese su nota: "))
+# if nota >= 6 :
+#     print("Aprobado")
+# else:
+#     print("Desaprobado")
+
+# Ejercicio 3
+# num = int(input("Ingrese un numero par: "))
+# if num % 2 == 0 :
+#     print("Ha ingresado un número par")
+# else:
+#     print("Por favor, ingrese un número par")
+
+# Ejercicio 4
+# edad = int(input("Ingrese su edad: "))
+# print("Pertenece a la categoria de edad: ", end="")
+# if edad < 12 :
+#     print("Niño/a")
+# elif edad >= 12 and edad < 18 :
+#     print("Adolecente")
+# elif edad >= 18 and edad < 30 :
+#     print("Adulto/a Joven")
+# else:
+#     print("Adulto/a")
+
+# Ejercicio 5
+contrasenia = len(input("Ingrese una contraseña: "))
+if contrasenia < 8 or contrasenia >14 :
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+else:
+    print("Ha ingresado una contraseña correcta")
