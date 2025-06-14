113dc20c2d05686610c71837ed89b033ed9bd396
Fernando <fernando.j.aguillon.b@gmail.com>
2025-06-13 22:49:22 -0300

    TP5

---
+++ 05_Funciones/FERNANDO-AGUILLON_TP-05.py
@@ -0,0 +1,52 @@
+# Ejercicio 1
+#multiplos = list(range(4, 101, 4))
+#print(multiplos)
+
+# Ejercicio 2
+# multiplos = list(range(4, 101, 4))
+# print(multiplos[-2])
+
+# Ejercicio 3
+# lista_vacia = []
+# lista_vacia.append("Hola")
+# lista_vacia.append("Mundo")
+# lista_vacia.append("Entero")
+# print(lista_vacia)
+
+# Ejercicio 4
+# animales = ["perro", "gato", "conejo", "pez"]
+# animales[1] = "loro"
+# animales[-1] = "oso"
+# print(animales)
+
+# Ejercicio 5
+# De una lista llamada "numeros" elige el de mayor valor con max(),
+# y luego lo remueve con .remove()
+# Por ultimo vuelve a imprimir la lista, pero ahora sin el valor eliminado
+
+# Ejercicio 6
+# numeros = list(range(10, 31, 5))
+# print(numeros[:2])
+
+# Ejercicio 7
+# autos = ["sedan", "polo", "suran", "gol"]
+# autos[1:3] = "Hola", "Mundo"
+# print(autos)
+
+# Ejercico 8
+# dobles = []
+# dobles.append(5 * 2)
+# dobles.append(10 * 2)
+# dobles.append(15 * 2)
+# print(dobles)
+
+# Ejercicio 9
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+# compras[2].append("jugo")
+# compras[1][1] = "tallarines"
+# compras[0].remove("pan")
+# print(compras)
+
+# Ejercicio 10
+lista_anidada = [15, True, [25.5, 27.9, 30.6], False]
+print(lista_anidada)
